1a060eb43427045daff8164cbfccc97e534be46a
Nguyen Hoang <nkhoang2@illinois.edu>
2025-11-06 16:42:52 -0600

    update data loading code

--- psibench/data_loader/main_loader.py
+++ psibench/data_loader/main_loader.py
@@ -3,7 +3,7 @@ from typing import Literal
 from datasets import load_dataset
 from data_loader.esc import load_esc_data, load_esc_data_with_indices
 
-def load_eeyore_data(dataset_type: Literal["ESC","HOPE", "AnnoMI"]):
+def load_eeyore_from_hf(dataset_type: Literal["ESC","HOPE", "AnnoMI"]):
     """Load ESC samples from the Eeyore dataset."""
     data = load_dataset("liusiyang/eeyore_profile", split='train', token=os.getenv("HF_TOKEN"))
     df = data.to_pandas()
@@ -23,24 +23,14 @@ def load_eeyore_dataset(dataset_type: str, indices: list = None):
         ValueError: If dataset_type is not supported
 
     """
-    match dataset_type.strip().lower():
+    match dataset_type:
         case "esc":
-            dataset_type = "esc"
+            if indices is not None:
+                return load_esc_data_with_indices(original_indices=indices)
+            return load_esc_data()
         case "hope":
-            dataset_type = "HOPE"
+            return load_eeyore_from_hf(dataset_type="HOPE")
         case "annomi":
-            dataset_type = "AnnoMI"
+            return load_eeyore_from_hf(dataset_type="AnnoMI")
         case _:
-            dataset_type = "Invalid input"
-
-
-    if dataset_type == "esc":
-        if indices is not None:
-            return load_esc_data_with_indices(original_indices=indices)
-        return load_esc_data()
-    elif dataset_type == "HOPE":
-        return load_eeyore_data(dataset_type="HOPE")
-    elif dataset_type == "AnnoMI":
-        return load_eeyore_data(dataset_type="AnnoMI")
-    else:
-        raise ValueError(f"Unsupported dataset type: {dataset_type}")
+            raise ValueError(f"Unsupported dataset type: {dataset_type}")
